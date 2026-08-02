bf4e0fc99d054c6408ca79ba3436a5052468ae88
42330642-blip <42330642@students.liu.edu.lb>
2026-08-02 15:50:37 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+CreateRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
