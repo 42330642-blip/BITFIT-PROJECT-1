665ffd117ef957acb372b9ef13ffaf86d2483ecc
42330642-blip <42330642@students.liu.edu.lb>
2026-08-02 15:52:55 +0300

    Plans.jsx

--- src/pages/Plans.jsx
+++ src/pages/Plans.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function Plans() {
+Function Plans() {
   const [height, setHeight] = useState('')
   const [weight, setWeight] = useState('')
   const [bmi, setBmi] = useState('')
