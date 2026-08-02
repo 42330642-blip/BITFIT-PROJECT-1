7777bb9e45e6cd85209d0fe075353ff455e27d68
42330642-blip <42330642@students.liu.edu.lb>
2026-08-02 15:52:16 +0300

    Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-function Contact() {
+Function Contact() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
